67d2f4450a88425edfc982c90c032543d074bae3
unknown <songyoungbin@gmail.com>
2019-03-13 00:06:41 +0900

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <landing-page></landing-page>
+    <data-binding></data-binding>
+  </div>
+</template>
+
+<script>
+  import LandingPage from '@/components/LandingPage'
+  import DataBinding from '@/components/DataBinding'
+
+  export default {
+    name: 'helloworld',
+    components: {
+      LandingPage , DataBinding
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/components/DataBinding.vue
@@ -0,0 +1,46 @@
+<template>
+  
+  <div id="wrapper">
+    <h1>11111111111111111111112222222222222222</h1>
+    <div class="container">
+        <h1>The multiplication table of 4.</h1>
+        <ul class="list-group">
+        
+            <li class="list-group-item" v-for="i in 11" :key="i">
+                {{ i }} times 4 equals {{ i * 4 }}.
+            </li>
+          
+        </ul>
+        <ul class="list-group">
+        
+            <li class="list-group-item" v-for="story in stories" :key="story">
+                오늘 뉴스 {{story}}.
+            </li>
+          
+        </ul>
+    </div>    
+  </div>
+</template>
+
+<script>
+// Full spec-compliant TodoMVC with localStorage persistence
+// and hash-based routing in ~120 effective lines of JavaScript.
+
+import Vue from 'vue'
+
+    new Vue({
+        el: '.container',
+        data: {
+          stories:[
+            "연세대",
+            "세브란스병원",
+            "응급실"
+          ]
+        }
+    })
+
+</script>
+
+<style>
+
+</style>
